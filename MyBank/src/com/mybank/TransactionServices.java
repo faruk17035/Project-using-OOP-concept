0878c2055d4ebda487399f20235920845565a93b
faruk17035 <faruk122564@gmail.com>
2023-11-05 06:49:09 +0600

    OOP Project using Java

---
+++ MyBank/src/com/mybank/TransactionServices.java
@@ -0,0 +1,19 @@
+package com.mybank;
+
+
+
+
+public interface TransactionServices {
+	
+	
+	
+	public void deposite(Account account,double ammount);
+	
+	public void withdrawn(Account account,double ammount);
+	
+	
+	
+
+	
+
+}
